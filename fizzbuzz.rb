9b481605aea8a559c5a6e23690f6d6bf11dbbf2a
Eva Smidt <shevangeline@gmail.com>
2018-01-27 20:37:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,20 +1,21 @@
 def fizzbuzz(int)
-#  if int % 3 == 0 && % 5 == 0
-#    "FizzBuzz"
-#  elsif int % 3 == 0
-#   "Fizz"
-  #else int % 5 == 0
-    # "Buzz"
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+  "Fizz"
+  elseif int % 5 == 0
+     "Buzz"
+  else int % 4 == 0
+    nil
 
-
-     if int % 3 == 0
-       "Fizz"
-     elsif int % 5 == 0
-      "Buzz"
-    elsif int % 3 == 0 && int % 5 == 0
-         "FizzBuzz"
-       else int % 4 == 0
-         nil
+    # if int % 3 == 0
+  #     "Fizz"
+    # elsif int % 5 == 0
+    #  "Buzz"
+  #  elsif int % 3 == 0 && int % 5 == 0
+    #     "FizzBuzz"
+    #   else int % 4 == 0
+    #     nil
 
 #  else int % 3 == 0 && % 5 == 0
   #  "FizzBuzz"
